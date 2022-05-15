6ced25ef208c32b120af7728f5af93bf782f58da
Sarvesh Waghmare <sarvesh.waghmare101@gmail.com>
2022-05-15 20:28:47 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.javaguides.login.bean.LoginBean;
+import net.javaguides.login.database.LoginDao;
+
+/**
+ * @email Ramesh Fadatare
+ */
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1 L;
+    private LoginDao loginDao;
+
+    public void init() {
+        loginDao = new LoginDao();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        LoginBean loginBean = new LoginBean();
+        loginBean.setUsername(username);
+        loginBean.setPassword(password);
+
+        try {
+            if (loginDao.validate(loginBean)) {
+                response.sendRedirect("loginsuccess.html");
+            } else {
+                HttpSession session = request.getSession();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
